adef383996ea0ee5762aa4f6c003ba435a753774
Daniel-29 <daniel.alex.barrera@hotmail.com>
2022-10-19 23:21:45 -0600

    Agregando 5 Test en App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,34 @@
 import { describe } from 'vitest'
+import {render, screen} from '@testing-library/react';
+import App from './App';
 
-describe('App', () => { 
-  it.todo('should render');
-  it.todo('should have title "Mi DUI es valido?"');
-  it.todo('should have an input text');
-  it.todo('should have a button with text "Validar"');
-  /* Lo que demas que se les ocurra que puedan probar */
+describe('App Tests', () => { 
+  it('should render', () => {
+    render(<App />);
+  });
+
+  it('should have title "Mi DUI es valido?"', () => {
+    render(<App />);
+    expect(screen.getByText('Mi DUI es valido?')).toBeInTheDocument();
+  });
+
+  it('should have an input text', () => {
+    render(<App />);
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
+  });
+
+  it('should have a button with text "Validar"', () => {
+    render(<App />);
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent('Validar');
+  });
+
+  it('button should have a event clickable', () => {
+    render(<App />);
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent('Validar');
+    button.click();
+  } );
 })
